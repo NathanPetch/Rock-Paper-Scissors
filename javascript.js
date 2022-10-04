4fc7b6ec93812c79253e634a11ef24ce40f0393a
NathanPetch <90180322+NathanPetch@users.noreply.github.com>
2022-10-04 16:12:57 +0100

    Made player selection case insensitive

--- javascript.js
+++ javascript.js
@@ -4,27 +4,25 @@ let gameOptions = ['rock', 'paper', 'scissors'];
 let random = Math.floor(Math.random() * gameOptions.length);
 return gameOptions[random];
 }
-console.log(getComputerChoice())
 /*
-Gather player selection and computers selection 
+Gather player selection (case insensitive) and computers selection 
 Compare selections to determine the winner
 Return string to player to alert if won or lost.
 */
-
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === 'rock' && computerSelection === 'paper'){
+    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
         return 'Computer has paper. You Lose'
-    } else if( playerSelection === 'paper' && computerSelection === 'scissors'){
+    } else if( playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors'){
         return 'Computer has scissors. You Lose'
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
         return 'Computer has rock. You Lose'
-    } else if (computerSelection === 'rock' && playerSelection === 'paper'){
+    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper'){
         return 'Computer has rock. You Win'
-    } else if( computerSelection === 'paper' && playerSelection === 'scissors'){
+    } else if( computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors'){
         return 'Computer has paper. You Win'
-    } else if (computerSelection === 'scissors' && playerSelection === 'rock'){
+    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'rock'){
         return 'Computer has scissors. You Win'
-    } else if(computerSelection === playerSelection){
+    } else if(computerSelection === playerSelection.toLowerCase()){
         return 'Its a tie'
     }
 }
