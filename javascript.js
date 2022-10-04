89b0ce29425857a99ca5c6188a6addca3fb719ba
NathanPetch <90180322+NathanPetch@users.noreply.github.com>
2022-10-04 15:48:22 +0100

    Added random computer selection for game (console only)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
-const myNum2 = 123;
-const myString2 = myNum2.toString();
-console.log(typeof myString2);
+// Choose random selection of rock, paper or scissors for computer
+function getComputerChoice(){
+let gameOptions = ['Rock', 'Paper', 'Scissors'];
+let random = Math.floor(Math.random() * gameOptions.length);
+return gameOptions[random];
+}
+console.log(getComputerChoice())
+/*
+Gather player selection and computers selection 
+Compare selections to determine the winner
+Return string to player to alert if won or lost.
+*/
+function playRound(playerSelection, computerSelection){
+    
+}
