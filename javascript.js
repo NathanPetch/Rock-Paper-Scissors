5451b1c75cade8888f1a6f19b1c2733a4a318abb
NathanPetch <90180322+NathanPetch@users.noreply.github.com>
2022-10-04 16:07:28 +0100

    added rock paper scissors winner deciphering

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // Choose random selection of rock, paper or scissors for computer
 function getComputerChoice(){
-let gameOptions = ['Rock', 'Paper', 'Scissors'];
+let gameOptions = ['rock', 'paper', 'scissors'];
 let random = Math.floor(Math.random() * gameOptions.length);
 return gameOptions[random];
 }
@@ -10,6 +10,21 @@ Gather player selection and computers selection
 Compare selections to determine the winner
 Return string to player to alert if won or lost.
 */
+
 function playRound(playerSelection, computerSelection){
-    
+    if (playerSelection === 'rock' && computerSelection === 'paper'){
+        return 'Computer has paper. You Lose'
+    } else if( playerSelection === 'paper' && computerSelection === 'scissors'){
+        return 'Computer has scissors. You Lose'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return 'Computer has rock. You Lose'
+    } else if (computerSelection === 'rock' && playerSelection === 'paper'){
+        return 'Computer has rock. You Win'
+    } else if( computerSelection === 'paper' && playerSelection === 'scissors'){
+        return 'Computer has paper. You Win'
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock'){
+        return 'Computer has scissors. You Win'
+    } else if(computerSelection === playerSelection){
+        return 'Its a tie'
+    }
 }
