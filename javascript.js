cc182b134ab6f03f7ff97b9f4c51ba67ad601c66
NathanPetch <90180322+NathanPetch@users.noreply.github.com>
2022-10-05 16:21:51 +0100

    changed game while loop logic

--- javascript.js
+++ javascript.js
@@ -11,25 +11,25 @@ Return string to player to alert if won or lost.
 warns player if they type in something that is not one of three options
 */
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() === 'rock' && computerSelection === 'paper'){
+    if (playerSelection === 'rock' && computerSelection === 'paper'){
         computerScoreCounter++
         return 'Computer has paper. You Lose' 
-    } else if( playerSelection.toLowerCase() === 'paper' && computerSelection === 'scissors'){
+    } else if( playerSelection === 'paper' && computerSelection === 'scissors'){
         computerScoreCounter++
         return 'Computer has scissors. You Lose' 
-    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection === 'rock'){
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
         computerScoreCounter++
         return 'Computer has rock. You Lose' 
-    } else if (computerSelection === 'rock' && playerSelection.toLowerCase() === 'paper'){
+    } else if (computerSelection === 'rock' && playerSelection === 'paper'){
         playerScoreCounter++
         return 'Computer has rock. You Win' 
-    } else if( computerSelection === 'paper' && playerSelection.toLowerCase() === 'scissors'){
+    } else if( computerSelection === 'paper' && playerSelection === 'scissors'){
         playerScoreCounter++
         return 'Computer has paper. You Win' 
-    } else if (computerSelection === 'scissors' && playerSelection.toLowerCase() === 'rock'){
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock'){
         playerScoreCounter++
         return 'Computer has scissors. You Win' 
-    } else if(computerSelection === playerSelection.toLowerCase()){
+    } else if(computerSelection === playerSelection){
         return 'Its a tie';
     } else if(playerSelection !== ['rock', 'paper', 'scissors']){
         return 'Please only type one of the three options'
@@ -42,7 +42,7 @@ let computerScoreCounter = 0
 //console.log(`The score is player ${playerScoreCounter} and computer ${computerScoreCounter}`)
 
 function game() {
-    let playerSelection = prompt('Please type Paper, Rock or Scissors?', '' | null)
+    let playerSelection = prompt('Please type Paper, Rock or Scissors?', '' | null).toLowerCase()
     let computerSelection = getComputerChoice()
     return playRound(playerSelection, computerSelection) 
     
@@ -50,7 +50,7 @@ function game() {
 /* first to five wins in rock paper scissors is played, 
 the console logs the winner of the round and what the current score is
 */
-  while (playerScoreCounter<2 || computerScoreCounter<2) {
+  while (playerScoreCounter<5 && computerScoreCounter<5) {
 	  console.log(game())
 	    console.log(`The score is: YOU - ${playerScoreCounter} COMPUTER - ${computerScoreCounter}`)	
 }
